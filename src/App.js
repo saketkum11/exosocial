99d810e747d579144395d052fd7d07fde7ed4be6
saketkum11 <kumarsaket601@gmail.com>
2022-06-12 21:43:53 +0530

    added: style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
 import logo from "./logo.png";
 import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+
 function App() {
   return (
     <div className="App">
       <Button variant="contained" color="secondary">
         material
       </Button>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Item>xs</Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>xs=6</Item>
+        </Grid>
+        <Grid item xs>
+          <Item>xs</Item>
+        </Grid>
+      </Grid>
     </div>
   );
 }
