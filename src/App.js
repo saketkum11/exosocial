d6dec638a3636f7dd664cd702948b63d44514d71
saketkum11 <kumarsaket601@gmail.com>
2022-06-13 08:48:48 +0530

    added: evn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import "./App.css";
-import logo from "./logo.png";
-import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
+import { MyRoutes } from "./Routes/Myroutes";
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained" color="secondary">
-        material
-      </Button>
-      <Grid container spacing={3}>
-        <Grid item xs>
-          <Item>xs</Item>
-        </Grid>
-        <Grid item xs={6}>
-          <Item>xs=6</Item>
-        </Grid>
-        <Grid item xs>
-          <Item>xs</Item>
-        </Grid>
-      </Grid>
+      <MyRoutes />
     </div>
   );
 }
---
+++ src/Routes/MyRoutes.jsx
@@ -0,0 +1,4 @@
+const MyRoutes = () => {
+  return <></>;
+};
+export { MyRoutes };
