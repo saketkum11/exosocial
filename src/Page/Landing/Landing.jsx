344886909567acb28a2b3a03b32483744f70d57b
saketkum11 <kumarsaket601@gmail.com>
2022-06-14 15:19:43 +0530

    added: style

--- src/Page/Landing/Landing.jsx
+++ src/Page/Landing/Landing.jsx
@@ -1,20 +1,28 @@
 import { Container, Grid, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
-const useStyles = makes;
+const useStyles = makeStyles({
+  btn: {
+    marginTop: "20px",
+  },
+  board: {
+    marginTop: "290",
+  },
+});
 
 const Landing = () => {
   const classes = useStyles();
   return (
     <>
       <Container>
-        <Grid container>
-          <Grid item xs variant="">
-            <Typography>ExoSocial</Typography>
+        <Grid container className={classes.board} spacing={3}>
+          <Grid item xs className={classes.btn}>
+            <Typography fontSize="3rem">ExoSocial</Typography>
+            <Typography fontSize="3rem">ExoSocial</Typography>
+            <Typography fontSize="3rem">ExoSocial</Typography>
           </Grid>
-          <Grid item xs={6}>
-            <div>
-              <img src="./assets/social.jpg" alt="" />
-            </div>
+          <Grid item xs={6} className={classes.btn}>
+            <img src="./assets/social.jpg" alt="" />
           </Grid>
         </Grid>
       </Container>
