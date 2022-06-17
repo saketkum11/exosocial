7270c3f50c56697af030694471b3360809505f4f
saketkum11 <kumarsaket601@gmail.com>
2022-06-17 10:53:16 +0530

    feat: auth

---
+++ src/features/counter/counter.js
@@ -0,0 +1,15 @@
+const initialState = {
+  value: 0,
+};
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+});
