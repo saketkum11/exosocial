8abfd8b3dd5bcf05597049b3bbda041401b7907f
saketkum11 <kumarsaket601@gmail.com>
2022-07-09 12:47:11 +0530

    added:commentController backend

--- src/server.js
+++ src/server.js
@@ -70,6 +70,27 @@ export function makeServer({ environment = "development" } = {}) {
       this.post("/posts/like/:postId", likePostHandler.bind(this));
       this.post("/posts/dislike/:postId", dislikePostHandler.bind(this));
 
+      //post comments routes (public)
+      this.get("/comments/:postId", getPostCommentsHandler.bind(this));
+
+      //post comments routes (private)
+      this.post("/comments/add/:postId", addPostCommentHandler.bind(this));
+      this.post(
+        "/comments/edit/:postId/:commentId",
+        editPostCommentHandler.bind(this)
+      );
+      this.post(
+        "/comments/delete/:postId/:commentId",
+        deletePostCommentHandler.bind(this)
+      );
+      this.post(
+        "/comments/upvote/:postId/:commentId",
+        upvotePostCommentHandler.bind(this)
+      );
+      this.post(
+        "/comments/downvote/:postId/:commentId",
+        downvotePostCommentHandler.bind(this)
+      );
       // user routes (public)
       this.get("/users", getAllUsersHandler.bind(this));
       this.get("/users/:username", getUserHandler.bind(this));
